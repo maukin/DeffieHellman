43e64215e01c0705bfdd33ebcb203562d44258cc
maukin <nikita.maukin@yandex.ru>
2021-10-16 10:53:01 +0300

    final

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ print("Public: \tn =", n)
 Alice_num = random.randint(0, 1000)
 Bob_num = random.randint(0, 1000)
 
-print("Private: \tAlice number =", Alice_num)
-print("Private: \tBob number =", Bob_num)
+print("Private: \tAlice's number =", Alice_num)
+print("Private: \tBob's number =", Bob_num)
 
 Alice_key1 = (g ** Alice_num) % n
 Bob_key1 = (g ** Bob_num) % n
 
-print("Private: \tAlice key1 =", Alice_key1)
-print("Private: \tBob key1 =", Bob_key1)
+print("Private: \tAlice's key1 =", Alice_key1)
+print("Private: \tBob's key1 =", Bob_key1)
 
-Alice_key_final = (g ** (Alice_key1 * Bob_key1)) % n
-Bob_key_final = (g ** (Alice_key1 * Bob_key1)) % n
+Alice_key_final = Bob_key1 ** Alice_num % n
+Bob_key_final = Alice_key1 ** Bob_num % n
 
-print("Public: \tAlice key final =", Alice_key_final)
-print("Public: \tBob key final =", Bob_key_final)
+print("Public: \tAlice's key final =", Alice_key_final)
+print("Public: \tBob's key final =", Bob_key_final)
 
 if Alice_key_final == Bob_key_final:
-    print("\t\tSuccess!")
+    print("\t\t Success!")
 else:
     print("Fail!")
 
